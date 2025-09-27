11e6a16297700bc5b020d2efb0c0f7d887b996c2
khoapham98 <phamanhkhoa9803@gmail.com>
2025-09-27 22:18:06 +0700

    testing dust sensor lib

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "inc/ads1115.h"
+#include "inc/gp2y1010au0f.h"
 
-#define 	FILE_PATH			"/dev/i2c-2"
-#define 	ADS1115_BASE_ADDR	0x48
+#define 	I2C_FILE_PATH			"/dev/i2c-2"
+#define 	GPIO_FILE_PATH			"/sys/class/gpio/gpio20"
 
 int main()
 {
-	int fd = ads1115_init(FILE_PATH, ADS1115_BASE_ADDR);
-	ads1115_config(fd, AIN0_GND, SINGLE_SHOT);
+	int i2c_fd = ads1115_init(I2C_FILE_PATH, ADS1115_BASE_ADDR);
+	ads1115_config(i2c_fd, AIN0_GND, SINGLE_SHOT);
+	gpio_set_mode(GPIO_FILE_PATH, GPIO_OUTPUT);
+	sleep(1);
 
 	while (1)
 	{
-		float val = ads1115_get_voltage(fd);
-		printf("%.2fV\n", val);
+		float res = gp2y_get_dust_density(i2c_fd);
+		printf("%.2f\n", res);
 		sleep(3);
 	}
 
