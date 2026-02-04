38d491c903988498f82eeec2aab78292d3c7d968
khoapham98 <phamanhkhoa9803@gmail.com>
2026-02-04 13:05:31 +0700

    add GPS data reading and synchronization with dust sensor

---
+++ main/gps/gps.h
@@ -0,0 +1,43 @@
+/**
+ * @file    gps.h
+ * @brief   ATGM336H driver header file
+ */
+#ifndef _GPS_H_
+#define _GPS_H_
+#include <stdint.h>
+
+/* Default latitude and longitude values.
+ * Here set to the coordinates of Ton Duc Thang University (TDTU), Ho Chi Minh City. 
+ */
+#define     DEFAULT_LATITUDE        10.7318f 
+#define     DEFAULT_LONGITUDE       106.6981f
+#define     DEFAULT_ALTITUDE        10
+
+#define     HOVER_SPEED_THRESHOLD_CM_S      20.0
+#define     HOVER_TIME_REQUIRED_SEC         4       
+
+typedef struct {
+    double lat;
+    double lon;
+    double alt;
+} gps_ctx_t;
+
+/**
+ * @brief   Check if drone is hovering based on horizontal velocity
+ * @return  none
+ */
+bool isDroneHovering(void);
+
+/**
+ * @brief   Read and parse MAVLink messages from GPS
+ * @return  none
+ */
+bool getGpsData(void);
+
+/**
+ * @brief   Initialize the UART interface for GPS communication
+ * @return  0 if success; -1 otherwise
+ */
+int gps_uart_init(void);
+
+#endif
